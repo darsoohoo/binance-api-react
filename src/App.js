e8b1e29a31da1443c5a66145dce84d888b0c1168
Darren SooHoo <darsoohoo@gmail.com>
2019-10-25 02:07:48 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom'
 import './App.css';
+import Navbar from './components/Navbar';
 import Landing from './components/Landing';
-import DigitalAssets from './components/DigitalAssets';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <header>
+        <Route exact path="/" component={Navbar} />
+        </header>
         <main>
           <Route exact path="/" component={Landing} />
          </main>
